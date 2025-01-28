c42a2cc3722b39f51db5513b38ebe62fb53ce572
Winston <hexwanderer@gmail.com>
2025-01-28 01:11:56 -0800

    update node.js version in docker, fixed styling issue

--- packages/client/src/components/Sidebar.tsx
+++ packages/client/src/components/Sidebar.tsx
@@ -337,7 +337,7 @@ export function NavUser({
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <Avatar className="h-8 w-8 rounded-lg">
+              <Avatar className="h-6 w-6 rounded-lg">
                 <AvatarImage src={user.avatar} alt={user.name} />
                 <AvatarFallback className="rounded-lg">CN</AvatarFallback>
               </Avatar>
